5939566ac3dab2f898eff903b1febd6c9555f2df
Famulski.Michal <187684@edu.p.lodz.pl>
2018-05-22 17:49:23 +0200

    Added estimated arrival time scenario and automated it

---
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/ArrivalTimeSteps.java
@@ -0,0 +1,35 @@
+package edu.iis.mto.bdd.trains.cucumber.steps;
+
+import java.util.List;
+
+import org.joda.time.LocalTime;
+
+import cucumber.api.PendingException;
+import cucumber.api.Transform;
+import cucumber.api.java.pl.Gdy;
+import cucumber.api.java.pl.Wtedy;
+import cucumber.api.java.pl.Zakładając;
+
+public class ArrivalTimeSteps {
+
+    @Zakładając("^chcę się dostać z \"(.*)\" do \"(.*)\"")
+    public void givenIWantToTravel(String departure, String destination) {
+        throw new PendingException();
+
+    }
+
+    @Zakładając("^następny pociąg odjeżdża o$ na linii \"(.*)\"")
+    public void nextTrainArrives(@Transform(JodaLocalTimeConverter.class) LocalTime startTime, String line) {
+        throw new PendingException();
+    }
+
+    @Gdy("^zapytam o godzinę przyjazdu")
+    public void whenIAskAboutArrivalTime() {
+        throw new PendingException();
+    }
+
+    @Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu: (.*)$")
+    public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
+        throw new PendingException();
+    }
+}
