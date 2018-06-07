58a92453e3a6b5f1e839003001ee5821984ad3bb
Famulski.Michal <187684@edu.p.lodz.pl>
2018-06-07 19:34:40 +0200

    Fixed glue code and feature code mismatches
    Signed-off-by: Famulski.Michal <187684@edu.p.lodz.pl>

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/ArrivalTimeSteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/ArrivalTimeSteps.java
@@ -12,23 +12,23 @@ import cucumber.api.java.pl.Zakładając;
 
 public class ArrivalTimeSteps {
 
-    @Zakładając("^chcę się dostać z \"(.*)\" do \"(.*)\"")
+    @Zakładając("^chcę się dostać z \"(.*)\" do \"(.*)\"$")
     public void givenIWantToTravel(String departure, String destination) {
         throw new PendingException();
 
     }
 
-    @Zakładając("^następny pociąg odjeżdża o$ na linii \"(.*)\"")
+    @Zakładając("^następny pociąg odjeżdża o (.*) na linii \"(.*)\"$")
     public void nextTrainArrives(@Transform(JodaLocalTimeConverter.class) LocalTime startTime, String line) {
         throw new PendingException();
     }
 
-    @Gdy("^zapytam o godzinę przyjazdu")
+    @Gdy("^zapytam o godzinę przyjazdu$")
     public void whenIAskAboutArrivalTime() {
         throw new PendingException();
     }
 
-    @Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu: (.*)$")
+    @Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu:$")
     public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
         throw new PendingException();
     }
