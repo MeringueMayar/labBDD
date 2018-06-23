1cb781be8241d11a401318caf90f24f949a4272c
dawid.liwocha <202214@edu.p.lodz.pl>
2018-06-23 12:54:43 +0200

    added times to scenario, changed delay from 15 to 30 min, updated unit test

--- src/test/java/edu/iis/mto/bdd/trains/junit/WhenCalculatingArrivalTimes.java
+++ src/test/java/edu/iis/mto/bdd/trains/junit/WhenCalculatingArrivalTimes.java
@@ -50,7 +50,7 @@ public class WhenCalculatingArrivalTimes {
         Mockito.when(timetableService.findLinesThrough("Parramatta", "Town Hall")).thenReturn(new ArrayList<Line>(Arrays.asList(line)));
         Mockito.when(timetableService.findArrivalTimes(line, "Parramatta")).thenReturn(arrivalTimes);
         List<LocalTime> result = intineraryService.findNextDepartures("Parramatta", "Town Hall", new LocalTime(8, 0));
-        assertThat(result, hasSize(3));
+        assertThat(result, hasSize(4));
     }
     
 }
