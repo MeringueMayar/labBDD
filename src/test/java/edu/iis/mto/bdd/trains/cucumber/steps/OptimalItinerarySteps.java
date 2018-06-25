7f43836485776104199ee6b3bf120a455173262b
Sebastian Piecowski <202268@edu.p.lodz.pl>
2018-06-25 17:28:34 +0200

    feature1_change, poprawione sporo bledow, usuniete zbedne importy, zmiana w pom wersji cucumber: java i junit - wyrzucalo exception

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -8,34 +8,42 @@ import org.joda.time.LocalTime;
 import org.junit.Assert;
 
 import cucumber.api.Transform;
+import cucumber.api.java.Before;
 import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
+import edu.iis.mto.bdd.trains.model.Line;
+import edu.iis.mto.bdd.trains.services.InMemoryTimetableService;
+import edu.iis.mto.bdd.trains.services.IntineraryService;
+import edu.iis.mto.bdd.trains.services.TimetableService;
 
 public class OptimalItinerarySteps {
     
+	TimetableService timeTableService;
+    IntineraryService itineraryService;
+    Line line;
 	List<LocalTime> arrivalTimes;
 	
+	@Before 
+	public void setUp() {
+		timeTableService=new InMemoryTimetableService();
+		itineraryService=new IntineraryService(timeTableService);
+	}
     @Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
-    public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
+    public void givenArrivingTrains(String lineName, String lineStart, String departure, String destination,
             @Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
-    	// throw new PendingException();
+    	line=Line.named(lineName).departingFrom(lineStart).withStations(lineStart, departure, destination);
     }
 
     @Gdy("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
     public void whenIWantToTravel(String departure, String destination,
             @Transform(JodaLocalTimeConverter.class) LocalTime startTime) {
-       // throw new PendingException();
+    	arrivalTimes=itineraryService.findNextDepartures(departure, departure, startTime);
     }
 
     @Wtedy("^powinienem uzyskać informację o pociągach o:$")
     public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
-        
-        arrivalTimes=new ArrayList<>();
-        arrivalTimes.add(new LocalTime("8:02"));
-        arrivalTimes.add(new LocalTime("8:11"));
-        arrivalTimes.add(new LocalTime("8:14"));
-        arrivalTimes.add(new LocalTime("8:21"));
+       
         Assert.assertThat(expectedTrainTimes.size(), Matchers.equalTo(arrivalTimes.size()));
         Assert.assertThat(expectedTrainTimes, Matchers.equalTo(arrivalTimes));
     
