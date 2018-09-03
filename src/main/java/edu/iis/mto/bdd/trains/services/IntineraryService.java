547e70858e726df945bd3d21b3d9f8fe812646c9
Klaudia Goczal <klaudia@lodz.pl>
2018-09-03 12:52:57 +0200

    feature1_impl Added @Then assertion and creadted IntineraryService class

---
+++ src/main/java/edu/iis/mto/bdd/trains/services/IntineraryService.java
@@ -0,0 +1,12 @@
+package edu.iis.mto.bdd.trains.services;
+
+import org.joda.time.LocalTime;
+
+import java.util.List;
+
+public class IntineraryService {
+
+    public List<LocalTime> findNextDepartures(String departure, String destination, LocalTime startTime) {
+        return null;
+    }
+}
--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -2,6 +2,7 @@ package edu.iis.mto.bdd.trains.cucumber.steps;
 
 import java.util.List;
 
+import edu.iis.mto.bdd.trains.services.IntineraryService;
 import org.joda.time.LocalTime;
 
 import cucumber.api.PendingException;
@@ -10,7 +11,15 @@ import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class OptimalItinerarySteps {
+
+    private IntineraryService intineraryService = new IntineraryService();
+    private LocalTime startTime;
+    private String destination;
+    private String departure;
 
     @Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
     public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
@@ -27,6 +36,6 @@ public class OptimalItinerarySteps {
 
     @Wtedy("^powinienem uzyskać informację o pociągach o:$")
     public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
-        throw new PendingException();
+        assertThat(intineraryService.findNextDepartures(departure, destination, startTime), is(expectedTrainTimes));
     }
 }
